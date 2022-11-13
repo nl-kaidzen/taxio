ae1450cef359d67d4e7c6529fa5ceb35c2242d15
Andrei Khabarov <akhabarov@qase.io>
2022-11-13 19:24:41 +0400

    Setup gh-pages deploy

--- src/App.tsx
+++ src/App.tsx
@@ -52,6 +52,10 @@ function App() {
       <Box
         component="main"
         sx={{
+            backgroundColor: (theme) =>
+                theme.palette.mode === 'light'
+                    ? theme.palette.grey[100]
+                    : theme.palette.grey[900],
           flexGrow: 1,
           height: '100vh',
           overflow: 'auto',
